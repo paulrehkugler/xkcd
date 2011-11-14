4df5c344bf649b1ed1509d75cfa739db30d755e1
Josh Bleecher Snyder <josharian+github@gmail.com>
2011-11-14 09:29:59 -0800

    More html cleaner tests

--- TestNSString_HTML.m
+++ TestNSString_HTML.m
@@ -19,6 +19,11 @@
                                          @"clichés", @"clich&eacute;s",
                                          @"House of Pancakes", @"<span style=\"color: #0000ED\">House</span> of Pancakes",
                                          @"bad > worse", @"bad > worse",
+                                         @"I Accidentally <noun>", @"I Accidentally <noun>",
+                                         @"<3", @"<3",
+                                         @"<>", @"<>",
+                                         @"RSS&M", @"RSS&M",
+                                         @"(or \\;;\"\\''{\\<<[' this mouseover text", @"(or \\;;\"\\''{\\<<[' this mouseover text",
                                          nil];
   for(NSString *snippet in htmlAndCleanedStrings) {
     NSString *cleaned = [htmlAndCleanedStrings objectForKey:snippet];
