f4cdcd508fa521757b2cfabf8ae218ad9763b94c
Paul Rehkugler <paulrehkugler@me.com>
2013-05-31 19:09:59 -0400

    Just use the bitmap (CG) width and height to make a CGSize.
    This was easier than I thought.

--- Classes/UIImage+EXIFCompensation.m
+++ Classes/UIImage+EXIFCompensation.m
@@ -11,7 +11,10 @@
 @implementation UIImage (EXIFCompensation)
 
 - (CGSize) exifAgnosticSize {
-  return CGSizeMake(9000, 9000);  // i'll get to this
+  int bitmapWidth = CGImageGetWidth(self.CGImage);
+  int bitmapHeight = CGImageGetHeight(self.CGImage);
+  
+  return CGSizeMake(bitmapWidth, bitmapHeight);
 }
 
 @end
