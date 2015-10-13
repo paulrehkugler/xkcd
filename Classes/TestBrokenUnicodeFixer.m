512c53121733fd8f02476c9f8aa8671e2efe6336
Paul Rehkugler <paulrehkugler@me.com>
2015-10-13 08:24:16 -0400

    Fix unit tests.

--- Classes/TestBrokenUnicodeFixer.m
+++ Classes/TestBrokenUnicodeFixer.m
@@ -2,50 +2,54 @@
 //  TestBrokenUnicodeFixer.m
 //
 
-//#import "TestBrokenUnicodeFixer.h"
-//#import "NSData+BrokenUnicodeJSON.h"
-//
-//#define CDATASTR(s) [NSData dataWithBytes:s length:sizeof(s)]
-//
-//@implementation TestBrokenUnicodeFixer
-//
-//- (void)assertDictionaryEntriesAreFixedCorrectly:(NSDictionary *)testCases {
-//  for(NSData *inputData in testCases) {
-//    NSData *expectedOutput = testCases[inputData];
-//    STAssertTrue([expectedOutput isKindOfClass:[NSData class]], @"Expected to get NSData object");
-//    STAssertEqualObjects([inputData dataByFixingFuckedUpUnicodeInJSON], expectedOutput,
-//                         @"Input '%@' should result in output '%@'.",
-//                         [[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding],
-//                         [[NSString alloc] initWithData:expectedOutput encoding:NSUTF8StringEncoding]);
-//  }
-//}
-//
-//- (void)testCleanData {
-//  [self assertDictionaryEntriesAreFixedCorrectly:@{
-//    CDATASTR("abcd"): CDATASTR("abcd"),
-//   }];
-//}
-//
-//- (void)testKnownBrokenData {
-//  [self assertDictionaryEntriesAreFixedCorrectly:@{
-//    CDATASTR("Erd\\u00c5\\u0091s"): CDATASTR("Erdős"),
-//    CDATASTR("Beyonc\\u00c3\\u00a9"): CDATASTR("Beyoncé"),
-//    CDATASTR("Nazg\\u00c8\\u0097l"): CDATASTR("Nazgȗl"),
-//    CDATASTR("E\\u00c3\\u00a4rendil"): CDATASTR("Eärendil"),
-//   }];
-//}
-//
-//- (void)testEdgeCases {
-//  [self assertDictionaryEntriesAreFixedCorrectly:@{
-//   CDATASTR(""): CDATASTR(""),
-//   CDATASTR("\\u00c5\\u0091"): CDATASTR("ő"),
-//   CDATASTR("\\u00c5\\u0091\\u00c5\\u0091"): CDATASTR("őő"),
-//   CDATASTR("\\u00x5\\u009p"): CDATASTR("\\u00x5\\u009p"),
-//   CDATASTR("\\u00x5\\u009p\\u00c5\\u0091"): CDATASTR("\\u00x5\\u009pő"),
-//   CDATASTR("\\u00x5\\u009p\\u00c5\\u0091\\u00x5\\u009p"): CDATASTR("\\u00x5\\u009pő\\u00x5\\u009p"),
-//   CDATASTR("\\u009"): CDATASTR("\\u009"),
-//   CDATASTR("\\u00"): CDATASTR("\\u00"),
-//   }];
-//}
-//
-//@end
+@import XCTest;
+#import "NSData+BrokenUnicodeJSON.h"
+
+#define CDATASTR(s) [NSData dataWithBytes:s length:sizeof(s)]
+
+@interface TestBrokenUnicodeFixer : XCTestCase
+
+@end
+
+@implementation TestBrokenUnicodeFixer
+
+- (void)assertDictionaryEntriesAreFixedCorrectly:(NSDictionary *)testCases {
+  for(NSData *inputData in testCases) {
+    NSData *expectedOutput = testCases[inputData];
+    XCTAssertTrue([expectedOutput isKindOfClass:[NSData class]], @"Expected to get NSData object");
+    XCTAssertEqualObjects([inputData dataByFixingFuckedUpUnicodeInJSON], expectedOutput,
+                         @"Input '%@' should result in output '%@'.",
+                         [[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding],
+                         [[NSString alloc] initWithData:expectedOutput encoding:NSUTF8StringEncoding]);
+  }
+}
+
+- (void)testCleanData {
+  [self assertDictionaryEntriesAreFixedCorrectly:@{
+    CDATASTR("abcd"): CDATASTR("abcd"),
+   }];
+}
+
+- (void)testKnownBrokenData {
+  [self assertDictionaryEntriesAreFixedCorrectly:@{
+    CDATASTR("Erd\\u00c5\\u0091s"): CDATASTR("Erdős"),
+    CDATASTR("Beyonc\\u00c3\\u00a9"): CDATASTR("Beyoncé"),
+    CDATASTR("Nazg\\u00c8\\u0097l"): CDATASTR("Nazgȗl"),
+    CDATASTR("E\\u00c3\\u00a4rendil"): CDATASTR("Eärendil"),
+   }];
+}
+
+- (void)testEdgeCases {
+  [self assertDictionaryEntriesAreFixedCorrectly:@{
+   CDATASTR(""): CDATASTR(""),
+   CDATASTR("\\u00c5\\u0091"): CDATASTR("ő"),
+   CDATASTR("\\u00c5\\u0091\\u00c5\\u0091"): CDATASTR("őő"),
+   CDATASTR("\\u00x5\\u009p"): CDATASTR("\\u00x5\\u009p"),
+   CDATASTR("\\u00x5\\u009p\\u00c5\\u0091"): CDATASTR("\\u00x5\\u009pő"),
+   CDATASTR("\\u00x5\\u009p\\u00c5\\u0091\\u00x5\\u009p"): CDATASTR("\\u00x5\\u009pő\\u00x5\\u009p"),
+   CDATASTR("\\u009"): CDATASTR("\\u009"),
+   CDATASTR("\\u00"): CDATASTR("\\u00"),
+   }];
+}
+
+@end
--- TestNSString_HTML.m
+++ TestNSString_HTML.m
@@ -2,30 +2,36 @@
 //  TestNSString_HTML.m
 //
 
-//#import "TestNSString_HTML.h"
-//#import "NSString_HTML.h"
-//
-//@implementation TestNSString_HTML
-//
-//- (void)testStringByCleaningHTML {
-//  NSDictionary *htmlAndCleanedStrings = @{
-//    @"<span>hi</span>": @"hi",
-//    @"good &gt; bad": @"good > bad",
-//    @"&quot;quote&quot;": @"\"quote\"",
-//    @"clich&eacute;s": @"clichés",
-//    @"<span style=\"color: #0000ED\">House</span> of Pancakes": @"House of Pancakes",
-//    @"bad > worse": @"bad > worse",
-//    @"I Accidentally <noun>": @"I Accidentally <noun>",
-//    @"<3": @"<3",
-//    @"<>": @"<>",
-//    @"RSS&M": @"RSS&M",
-//    @"(or \\;;\"\\''{\\<<[' this mouseover text": @"(or \\;;\"\\''{\\<<[' this mouseover text"
-//  };
-//
-//  for(NSString *snippet in htmlAndCleanedStrings) {
-//    NSString *cleaned = htmlAndCleanedStrings[snippet];
-//    STAssertEqualObjects([NSString stringByCleaningHTML:snippet], cleaned, @"Snippet '%@' should result in clean string '%@'.", snippet, cleaned);
-//  }
-//}
-//
-//@end
+@import XCTest;
+@import UIKit;
+
+#import "NSString_HTML.h"
+
+@interface TestNSString_HTML : XCTestCase
+
+@end
+
+@implementation TestNSString_HTML
+
+- (void)testStringByCleaningHTML {
+  NSDictionary *htmlAndCleanedStrings = @{
+    @"<span>hi</span>": @"hi",
+    @"good &gt; bad": @"good > bad",
+    @"&quot;quote&quot;": @"\"quote\"",
+    @"clich&eacute;s": @"clichés",
+    @"<span style=\"color: #0000ED\">House</span> of Pancakes": @"House of Pancakes",
+    @"bad > worse": @"bad > worse",
+    @"I Accidentally <noun>": @"I Accidentally <noun>",
+    @"<3": @"<3",
+    @"<>": @"<>",
+    @"RSS&M": @"RSS&M",
+    @"(or \\;;\"\\''{\\<<[' this mouseover text": @"(or \\;;\"\\''{\\<<[' this mouseover text"
+  };
+
+  for(NSString *snippet in htmlAndCleanedStrings) {
+    NSString *cleaned = htmlAndCleanedStrings[snippet];
+    XCTAssertEqualObjects([NSString stringByCleaningHTML:snippet], cleaned, @"Snippet '%@' should result in clean string '%@'.", snippet, cleaned);
+  }
+}
+
+@end
