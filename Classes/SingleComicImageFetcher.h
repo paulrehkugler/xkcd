653a97a33de10660470cba570abc490eb2e6fa5a
Paul Rehkugler <paul@tumblr.com>
2015-12-28 12:43:54 -0500

    Add a comment.

--- Classes/SingleComicImageFetcher.h
+++ Classes/SingleComicImageFetcher.h
@@ -11,6 +11,15 @@
 
 @interface SingleComicImageFetcher : NSObject
 
+/**
+ *  Fetches an image for the supplied comic.
+ *
+ *  @param comic   The Comic to fetch an image for.
+ *  @param context An arbitrary @c id type.
+ *
+ *  @note @c context is currently used as a flag to open the comic after it is downloaded.
+ *  Probably want to rethink this architecture in the future.
+ */
 - (void)fetchImageForComic:(Comic *)comic context:(id)context;
 - (void)fetchImagesForAllComics;
 - (BOOL)downloadingAll;
